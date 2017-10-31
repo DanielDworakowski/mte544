ebc9b99b200eb4ce0eb08e36055c39b8a073b695
Daniel Dworakowski <ddworako@edu.uwaterloo.ca>
2017-10-30 21:43:20 -0400

    fixwq

--- workspace/src/turtlebot_example/src/particleFilterNode.cpp
+++ workspace/src/turtlebot_example/src/particleFilterNode.cpp
@@ -18,6 +18,7 @@
 #include <tf/transform_datatypes.h>
 #include <gazebo_msgs/ModelStates.h>
 #include <visualization_msgs/Marker.h>
+#include <geometry_msgs/PoseWithCovarianceStamped.h>
 #include <nav_msgs/OccupancyGrid.h>
 #include "Eigen/Dense"
 #include <mutex>
